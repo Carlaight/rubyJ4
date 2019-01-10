3f3d049ef558ff5a146f4880888d7dff88df1b81
carlaight <charlesmix23@gmail.com>
2019-01-10 22:14:23 +0100

    check

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -31,34 +31,8 @@ def full_pyramid
         end
 end
 
+
 def wtf_pyramid
-    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-    print "> Un chiffre impair: "
-    floors = gets.chomp.to_i
-    nbfloors = (floors - 1)
-    space = " "
-    n  = 1
-     
-    if  floors % 2 != 0
-        puts " Voilàààààà"
-        while (n <= floors*2)
-            print space * nbfloors
-            puts "#"* n
-            n = n + 2
-            nbfloors = nbfloors - 1
-        end
-
-        while (n > floors*2)
-        print space * nbfloors
-        puts "#"* n
-        n = n - 1
-        nbfloors = nbfloors + 1
-        end
-    else puts"Un nombre impair !"    
-    end
-end
-
-def half_pyramid
 
         puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (entre 1 et 25)"
         print ">"
@@ -72,3 +46,11 @@ def half_pyramid
             pyramid2.(level.to_i)
     
         end
+
+def perform
+    half_pyramid
+    full_pyramid
+    wtf_pyramid
+end
+
+perform
